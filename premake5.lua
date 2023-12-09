dcde046c0f4cedc08fd02d37a94f1f8a6ac4fc3c
eanigmal <boncoi19990@gmail.com>
2023-12-09 22:37:24 +0700

    add imgui

--- premake5.lua
+++ premake5.lua
@@ -1,31 +1,31 @@
 workspace"Sandbox"
-	location"../"
+	location"./"
 	configurations{"Debug","Release"}
 
-project"Sanbox"
-	location"build/"
-	kind"ConsoleApp"
-	language"C++"
-	files{
-		"src/**.cpp",
-		"src/**.c",
-		"inc/**.hpp",
-		"inc/**.h",
-	}
-	includedirs"inc/"
+	project"Sandbox"
+		location"build/"
+		kind"ConsoleApp"
+		language"C++"
+		files{
+			"src/**.cpp",
+			"src/**.c",
+			"inc/**.hpp",
+			"inc/**.h",
+		}
+		includedirs"inc/"
 
-	libdirs"lib/"
-	links{"glfw3dll", "assimp"}
+		libdirs"lib/"
+		links{"glfw3dll", "assimp"}
 
-	targetdir"bin/"
-	targetname"Sandbox"
+		targetdir"bin/"
+		targetname"Sandbox"
 
-	filter"configurations:Debug"
-	defines{"DEBUG"}
-	symbols"On"
+		filter"configurations:Debug"
+		defines{"DEBUG"}
+		symbols"On"
 
-	filter"configurations:Release"
-	defines{"RELEASE"}
-	optimize"On"
+		filter"configurations:Release"
+		defines{"RELEASE"}
+		optimize"On"
 
-	filter{}
+		filter{}
